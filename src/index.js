51bbf9831a6a21cb4aa1b4ad6e4784c39a2f8f91
Jeffrei Cher <jeffreicher@yahoo.com>
2018-03-16 14:48:47 -0700

    React intro prototype completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const user = {
+    firstName: 'Jeffrei',
+    lastName: 'Cher',
+    luckyNumber: luckyNumber()
+}
+
+function luckyNumber(){
+    return Math.floor((Math.random() * 1000) +1);
+}
+
+function greeting(user){
+    return (
+    <div className='container'>
+        <h1>Hello, my name is {user.firstName}</h1>
+        <h2 className='text-muted'>Your lucky number is: {user.luckyNumber}</h2>
+    </div>
+    );
+}
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
+
