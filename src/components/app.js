cceb3658a5c442a645382cb8a09e33dac83bfd25
Jeffrei Cher <jeffreicher@yahoo.com>
2018-03-19 20:46:33 -0700

    Completed FS2

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Stopwatch from './stopwatch';
 
 const App = (props) => {
     return (
         <div className='container'>
             <h1 className='text-center'>Stopwatch</h1>
+            <Stopwatch />
         </div>
     );
 }
