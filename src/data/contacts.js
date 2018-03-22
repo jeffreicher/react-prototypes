6ff9ccde3a3b9039435e98c00bb728ab82318adc
Jeffrei Cher <jeffreicher@yahoo.com>
2018-03-22 16:41:44 -0700

    Completed FS2

---
+++ src/data/contacts.js
@@ -0,0 +1,6 @@
+export default [
+    {firstName: 'Jeffrei', lastName:'Cher', phone: '123-456-7890', email: 'youalreadyknowtho@swag.com'},
+    {firstName: 'Parker', lastName:'Nowayinhell', phone: '123-456-7890', email: 'reallylonglastname@goddamn.com'},
+    {firstName: 'Matt', lastName:'Father', phone: '123-456-7890', email: 'callmedaddy@home.com'},
+    {firstName: 'Nolan', lastName:'Kingofburgers', phone: '123-456-7890', email: 'allhailtheking@burgers.com'}
+ ];
