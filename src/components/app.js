d8237ebf2d80a290d5d7bc653a89c6e0cc43ca18
Jeffrei Cher <jeffreicher@yahoo.com>
2018-03-22 23:59:57 -0700

    Changed App to class component so that it can recieve contacts

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ContactList from './contact_list';
 import ContactForm from './contact_form';
+import contactData from '../data/contacts';
 
-export default () => {
-    return (
-        <div className="container">
-            <h1 className="text-center">Address Book</h1>
-            <div className="row">
-            <div className="col-4">
-                <ContactForm />
+class App extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            contacts: contactData
+        }
+    }
+    render(){
+        return (
+            <div className="container">
+                <h1 className="text-center">Address Book</h1>
+                <div className="row">
+                <div className="col-4">
+                    <ContactForm />
+                </div>
+                    <ContactList contacts={this.state.contacts}/>
+                </div>            
             </div>
-                <ContactList />
-            </div>            
-        </div>
-    )    
+        )    
+    }
 }
+
+export default App;
+
--- src/components/contact_list.js
+++ src/components/contact_list.js
@@ -1,18 +1,13 @@
 import React, { Component }from 'react';
 import ContactCard from './contact_card';
-import contactData from '../data/contacts';
 
 class ContactList extends Component {
     constructor(props){
         super(props)
-
-        this.state = {
-            contacts: contactData
-        }
     }
 
     render(){
-        const list = this.state.contacts.map((item, index) => {
+        const list = this.props.contacts.map((item, index) => {
             return <ContactCard key={index} contact={item} />
         });
         return (
