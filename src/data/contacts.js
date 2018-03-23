8602cbedee624e2cada8286196dee987d8d70ec3
Jeffrei Cher <jeffreicher@yahoo.com>
2018-03-22 23:47:52 -0700

    contacts fix

--- src/data/contacts.js
+++ src/data/contacts.js
@@ -1,6 +1,6 @@
 export default [
     {firstName: 'Jeffrei', lastName:'Cher', phone: '123-456-7890', email: 'youalreadyknowtho@swag.com'},
-    {firstName: 'Parker', lastName:'Nowayinhell', phone: '123-456-7890', email: 'reallylonglastname@goddamn.com'},
+    {firstName: 'Parker', lastName:'Nowayinhell', phone: '123-445-7890', email: 'reallylonglastname@goddamn.com'},
     {firstName: 'Matt', lastName:'Father', phone: '123-456-7890', email: 'callmedaddy@home.com'},
     {firstName: 'Nolan', lastName:'Kingofburgers', phone: '123-456-7890', email: 'allhailtheking@burgers.com'}
  ];
