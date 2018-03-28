d46222f9d6025f0c26808fb121d61a8b32756f75
Jeffrei Cher <jeffreicher@yahoo.com>
2018-03-27 19:55:03 -0700

    completed fs1

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,13 @@
+import React from 'react';
+import './css/app.css';
 
+const App = () => {
+    return (
+        <div className="main-container">
+            <h1 className="main-title">Scrapbook</h1>
+        </div>
+    );
+}
+
+export default App;
 
