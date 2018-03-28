b39e92aa836a1df0a8c1dac943b006549868263c
Jeffrei Cher <jeffreicher@yahoo.com>
2018-03-27 20:39:29 -0700

    finished FS4

--- src/components/image_data.js
+++ src/components/image_data.js
@@ -10,34 +10,63 @@ import Weed8 from './images/weed8.jpg';
 export default [
     {
         src:Weed1,
-        style:{}
+        style:{
+            left: '5%',
+            transform: 'rotate(-10deg)'
+        }
     },
     {
         src:Weed2,
-        style:{}
+        style:{
+            left: '40%',
+            transform: 'rotate(6deg)'
+        }
     },
     {
         src:Weed3,
-        style:{}
+        style:{
+            right: '15%',
+            transform: 'rotate(-9deg)'
+        }
     },
     {
         src:Weed4,
-        style:{}
+        style:{
+            left: '8%',
+            top: '36%',
+            transform: 'rotate(8deg)'
+        }
     },
     {
         src:Weed5,
-        style:{}
+        style:{
+            right: '12%',
+            top: '26%',
+            transform: 'rotate(-12deg)'
+        }
     },
     {
         src:Weed6,
-        style:{}
+        style:{
+            right: '42%',
+            top: '31%',
+            transform: 'rotate(10deg)'
+        }
     },
     {
         src:Weed7,
-        style:{}
+        style:{
+            left: '22%',
+            top: '52%',
+            transform: 'rotate(-5deg)'
+        }
     },    
     {
         src:Weed8,
-        style:{}
+        style:{
+            right: '22%',
+            top: '62%',
+            transform: 'rotate(16deg)'
+        }
     }
 ]
--- src/components/scrapbook_images.js
+++ src/components/scrapbook_images.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const ScrapImages = (props) => {
-    const {src} = props.about;
+    const {src, style} = props.about;
     return (
-        <img className="scrapbook-image" src={src} />
+        <img className="scrapbook-image" src={src} style={style}/>
     );
 }
 export default ScrapImages;
