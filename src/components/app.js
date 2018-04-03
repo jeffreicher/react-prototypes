d2f43c91c3c25dedce296ad4c129b0893b29b989
Jeffrei Cher <jeffreicher@yahoo.com>
2018-04-02 18:03:45 -0700

    Completed FS2

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,14 @@
+import React from 'react';
+import Clock from './clock';
 
+const App = () => {
+    return (
+        <div className="container">
+            <h1 className="text-center">REDUX CLOCK</h1>
+            <Clock />
+        </div>
+    );
+};
+
+export default App;
 
